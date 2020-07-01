96be119f2c66f5e3144da80e55ec6634cf19746a
Carlos Rafael <quipan@gmail.com>
2020-07-01 16:16:19 -0500

    Definicion de archivos de rutas

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,4 +3,7 @@ module.exports = (app) =>{
     require('./consultas')(app),
     require('./comentarios')(app),
     require('./sitiosinteres')(app)
+    require('./personal')(app)
+    require('./actividadcampo')(app)
+    require('./ingreso')(app)
 };
